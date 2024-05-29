f6894e962f1a91719585491eaff818eae0068f8c
Gegë Dobruna <gegedobruna@mailbox.unideb.hu>
2024-05-29 18:44:11 +0200

    build: created classes for the  structure of the project

---
+++ src/main/java/puzzle/solver/Solver.java
@@ -0,0 +1,11 @@
+package puzzle.solver;
+
+import puzzle.ChessMove;
+import puzzle.ChessState;
+import puzzle.solver.BreadthFirstSearch;
+
+public class Solver {
+
+    //TODO: Implement the Solver class using the BreadthFirstSearch class
+
+}
