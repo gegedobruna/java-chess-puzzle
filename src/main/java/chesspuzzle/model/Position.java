49f7e9991e292f09886e003de996d69886941d6e
Gegë Dobruna <gegedobruna@mailbox.unideb.hu>
2024-06-01 14:42:50 +0200

    build: Completed Position class and its appropriate test class

--- src/main/java/chesspuzzle/model/Position.java
+++ src/main/java/chesspuzzle/model/Position.java
@@ -1,5 +1,15 @@
 package chesspuzzle.model;
 
-public record Position() {
+public record Position(int row, int col) {
 
+    public Position move(Direction direction) {
+        return new Position(row + direction.getRowChange(), col + direction.getColChange());
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", row, col);
+    }
 }
