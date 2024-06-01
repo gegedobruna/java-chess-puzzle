f61e43259dd3456b45e09987c093d658741b5f70
Gegë Dobruna <gegedobruna@mailbox.unideb.hu>
2024-06-01 13:04:21 +0200

    build: Refined Direction class and created appropriate test class

--- src/main/java/chesspuzzle/solver/Solver.java
+++ src/main/java/chesspuzzle/solver/Solver.java
@@ -1,6 +1,15 @@
 package chesspuzzle.solver;
+
+import chesspuzzle.model.ChessState;
+import chesspuzzle.model.Position;
+import puzzle.TwoPhaseMoveState;
 import puzzle.solver.BreadthFirstSearch;
 
-public class Solver {
 
+public class Solver {
+    public static void main(String[] args) {
+        var bfs = new BreadthFirstSearch<TwoPhaseMoveState.TwoPhaseMove<Position>>();
+        ChessState initialState = new ChessState();
+        bfs.solveAndPrintSolution(initialState);
+    }
 }
