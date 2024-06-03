517393df2e22e6bd6a21e3db7e791828aefe5fd0
Gegë Dobruna <gegedobruna@mailbox.unideb.hu>
2024-06-03 22:58:10 +0200

    test: added unit test class for ChessState class.

--- src/main/java/chesspuzzle/game/GameApplication.java
+++ src/main/java/chesspuzzle/game/GameApplication.java
@@ -1,11 +1,24 @@
 package chesspuzzle.game;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class GameApplication extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+import java.io.IOException;
+import java.util.Objects;
 
+public class GameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/game.fxml")));
+        stage.setTitle("JavaFX Chess Puzzle");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
+
 }
