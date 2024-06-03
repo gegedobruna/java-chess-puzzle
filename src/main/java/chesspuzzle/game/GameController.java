876e870c9f58e0f167417d94bc172dcd8d56ee04
Gege Dobruna <153127699+gegedobruna@users.noreply.github.com>
2024-06-03 23:46:27 +0200

    Build: Implemented GameController class and related functionality

--- src/main/java/chesspuzzle/game/GameController.java
+++ src/main/java/chesspuzzle/game/GameController.java
@@ -6,10 +6,18 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import org.tinylog.Logger;
 import chesspuzzle.model.ChessState;
 import chesspuzzle.model.Position;
 import puzzle.TwoPhaseMoveState;
@@ -21,87 +29,141 @@ public class GameController {
     private final ImageStorage<Integer> imageStorage = new OrdinalImageStorage("/chesspieces",
             "king.png",
             "knight.png");
-
+    private final IntegerProperty numberOfMoves = new SimpleIntegerProperty(0);
+    private final ReadOnlyObjectWrapper<Position> highlightedPosition = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<Position> goalPosition = new ReadOnlyObjectWrapper<>(new Position(0, 6));
     @FXML
     private GridPane grid;
     @FXML
     private TextField numberOfMovesField;
-
     private ChessState state;
-    private final IntegerProperty numberOfMoves = new SimpleIntegerProperty(0);
-    private final ReadOnlyObjectWrapper<Position> highlightedPosition = new ReadOnlyObjectWrapper<>();
-    private final ReadOnlyObjectWrapper<Position> goalPosition = new ReadOnlyObjectWrapper<>();
 
     @FXML
     private void initialize() {
-        // TODO: Bind numberOfMovesField to numberOfMoves
-        // TODO: Create initial game state
-        // TODO: Clear and populate grid
-        // TODO: Add listeners to state properties
+        numberOfMovesField.textProperty().bind(numberOfMoves.asString());
+        createState();
+        clearAndPopulateGrid();
+        registerKeyEventHandler();
+        state.kingPositionProperty().addListener((observable, oldPosition, newPosition) -> clearAndPopulateGrid());
+        state.knightPositionProperty().addListener((observable, oldPosition, newPosition) -> clearAndPopulateGrid());
     }
 
     private void restartGame() {
-        // TODO: Create initial game state
-        // TODO: Reset number of moves
-        // TODO: Clear and populate grid
+        createState();
+        numberOfMoves.set(0);
+        clearAndPopulateGrid();
     }
 
     private void createState() {
-        // TODO: Initialize state with a new ChessState
-        // TODO: Reset highlightedPosition to null
+        state = new ChessState();
+        state.solvedProperty().addListener(this::handleSolved);
+        highlightedPosition.set(null);
     }
 
     private void handleSolved(ObservableValue<? extends Boolean> observableValue, Boolean oldValue, Boolean newValue) {
-        // TODO: If puzzle is solved (newValue is true), show solved alert
+        if (newValue) {
+            Platform.runLater(this::showSolvedAlert);
+        }
     }
 
     private void showSolvedAlert() {
-        // TODO: Create an alert with "Game Over" header
-        // TODO: Set content text to "Congratulations, you have solved the puzzle!"
-        // TODO: Show the alert and wait for user response
-        // TODO: Restart the game
+        var alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Game Over");
+        alert.setContentText("Congratulations, you have solved the puzzle!");
+        alert.showAndWait().ifPresent(response -> restartGame());
     }
 
     @FXML
     private void handleKeyPress(KeyEvent keyEvent) {
-        // TODO: Check if Ctrl+R is pressed to restart the game
-        // TODO: Check if Ctrl+Q is pressed to exit the application
+        var restartKeyCombination = new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN);
+        var quitKeyCombination = new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN);
+        if (restartKeyCombination.match(keyEvent)) {
+            Logger.debug("Restarting game");
+            restartGame();
+        } else if (quitKeyCombination.match(keyEvent)) {
+            Logger.debug("Exiting");
+            Platform.exit();
+        }
+    }
+
+    private void registerKeyEventHandler() {
+        Platform.runLater(() -> grid.getScene().setOnKeyPressed(this::handleKeyPress));
     }
 
     @FXML
     private void handleMouseClick(MouseEvent event) {
-        // TODO: Get source node from event
-        // TODO: Calculate row and column from GridPane indices
-        // TODO: Log the click position
-        // TODO: Handle the click on the specific row and column
+        var source = (Node) event.getSource();
+        var row = ChessState.BOARD_SIZE - 1 - GridPane.getRowIndex(source);
+        var col = GridPane.getColumnIndex(source);
+        Logger.debug("Click on square ({},{})", row, col);
+        handleClickOn(row, col);
     }
 
     private void handleClickOn(int row, int col) {
-        // TODO: Create position object from row and col
-        // TODO: If no piece is selected, check if clicked position has a piece that can be moved
-        // TODO: If a piece is selected, check if move is legal and make the move
-        // TODO: Clear and populate grid
+        Position clickedPosition = new Position(row, col);
+
+        if (highlightedPosition.get() == null) {
+            state.getPieceAt(row, col).ifPresent(pieceIndex -> {
+                if (state.isLegalToMoveFrom(clickedPosition)) {
+                    highlightedPosition.set(clickedPosition);
+                    clearAndPopulateGrid();
+                }
+            });
+        } else {
+            TwoPhaseMoveState.TwoPhaseMove<Position> move = new TwoPhaseMoveState.TwoPhaseMove<>(highlightedPosition.get(), clickedPosition);
+            if (state.isLegalMove(move)) {
+                makeMove(move);
+            } else {
+                Logger.error("Illegal move attempted: from {} to {}", highlightedPosition.get(), clickedPosition);
+                highlightedPosition.set(null);
+            }
+            clearAndPopulateGrid();
+        }
     }
 
     private void makeMove(TwoPhaseMoveState.TwoPhaseMove<Position> move) {
-        // TODO: Make the move in the game state
-        // TODO: Increment the number of moves
-        // TODO: Deselect the highlighted piece
+        state.makeMove(move);
+        numberOfMoves.set(numberOfMoves.get() + 1);
+        highlightedPosition.set(null);
+        clearAndPopulateGrid();
     }
 
     private void clearAndPopulateGrid() {
-        // TODO: Clear all children from the grid
-        // TODO: Iterate over board rows and columns
-        // TODO: Create and add square to the grid for each position
+        grid.getChildren().clear();
+
+        for (var row = 0; row < ChessState.BOARD_SIZE; row++) {
+            for (var col = 0; col < ChessState.BOARD_SIZE; col++) {
+                grid.add(createSquare(row, col), col, ChessState.BOARD_SIZE - 1 - row);
+            }
+        }
     }
 
     private StackPane createSquare(int row, int col) {
-        // TODO: Create a new StackPane
-        // TODO: Apply CSS class for light or dark square
-        // TODO: Highlight square if it is selected or can move
-        // TODO: Add piece image to square if present
-        // TODO: Set click handler for the square
-        // TODO: Return the square
-        return null;
+        var square = new StackPane();
+
+        if (col % 2 == row % 2) {
+            square.getStyleClass().add("light-square");
+        } else {
+            square.getStyleClass().add("dark-square");
+        }
+
+        Position currentPosition = new Position(row, col);
+        if (currentPosition.equals(highlightedPosition.get())) {
+            square.getStyleClass().add("highlighted-square");
+        } else if (state.isLegalToMoveFrom(currentPosition)) {
+            square.getStyleClass().add("legal-move-square");
+        } else if (currentPosition.equals(goalPosition.get())) {
+            square.getStyleClass().add("goal-square");
+        }
+
+        state.getPieceAt(row, col).flatMap(imageStorage::get).ifPresent(image -> {
+            var imageView = new ImageView(image);
+            imageView.getStyleClass().add("image-view");
+            imageView.setVisible(true);
+            square.getChildren().add(imageView);
+        });
+
+        square.setOnMouseClicked(this::handleMouseClick);
+        return square;
     }
 }
--- src/main/java/chesspuzzle/model/ChessState.java
+++ src/main/java/chesspuzzle/model/ChessState.java
@@ -1,79 +1,90 @@
 package chesspuzzle.model;
 
-
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import lombok.Getter;
 import puzzle.TwoPhaseMoveState;
 
-import lombok.Getter;
 import java.util.*;
 
 @Getter
 public class ChessState implements TwoPhaseMoveState<Position> {
     public static final int BOARD_SIZE = 8;
 
-    private Position kingPosition;
-    private Position knightPosition;
+    private final ReadOnlyObjectWrapper<Position> kingPosition = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyObjectWrapper<Position> knightPosition = new ReadOnlyObjectWrapper<>();
+    private final ReadOnlyBooleanWrapper solved = new ReadOnlyBooleanWrapper();
     private final List<String[]> board;
 
-    // Constructors
     public ChessState() {
         this(new Position(2, 1), new Position(2, 2));
     }
 
     public ChessState(Position kingPosition, Position knightPosition) {
-        this.kingPosition = kingPosition;
-        this.knightPosition = knightPosition;
+        this.kingPosition.set(kingPosition);
+        this.knightPosition.set(knightPosition);
         this.board = new ArrayList<>(BOARD_SIZE);
+        Position goalPosition = new Position(0, 6);
         initializeBoard();
+        solved.bind(this.kingPosition.isEqualTo(goalPosition).or(this.knightPosition.isEqualTo(goalPosition)));
     }
 
-    // Private helper methods
     private void initializeBoard() {
         for (int i = 0; i < BOARD_SIZE; i++) {
             String[] row = new String[BOARD_SIZE];
             Arrays.fill(row, ".");
             board.add(row);
         }
-        board.get(kingPosition.row())[kingPosition.col()] = "K";
-        board.get(knightPosition.row())[knightPosition.col()] = "N";
+        board.get(kingPosition.get().row())[kingPosition.get().col()] = "K";
+        board.get(knightPosition.get().row())[knightPosition.get().col()] = "N";
     }
 
-    private boolean isValid(Position position) {
+    public Optional<Integer> getPieceAt(int row, int col) {
+        String piece = board.get(row)[col];
+        return switch (piece) {
+            case "K" -> Optional.of(0);
+            case "N" -> Optional.of(1);
+            default -> Optional.empty();
+        };
+    }
+
+    boolean isValid(Position position) {
         return position.row() >= 0 && position.row() < BOARD_SIZE && position.col() >= 0 && position.col() < BOARD_SIZE;
     }
 
-    private boolean isValidKingMove(Position from, Position to) {
+    public boolean isValidKingMove(Position from, Position to) {
         int dr = Math.abs(from.row() - to.row());
         int dc = Math.abs(from.col() - to.col());
-        return (dr <= 1 && dc <= 1);
+        return dr <= 1 && dc <= 1;
     }
 
-    private boolean isValidKnightMove(Position from, Position to) {
+    public boolean isValidKnightMove(Position from, Position to) {
         int dr = Math.abs(from.row() - to.row());
         int dc = Math.abs(from.col() - to.col());
         return (dr == 2 && dc == 1) || (dr == 1 && dc == 2);
     }
 
-    private boolean isUnderAttack(Position piecePosition, Position attackerPosition) {
-        if (piecePosition.equals(kingPosition)) {
+    boolean isUnderAttack(Position piecePosition, Position attackerPosition) {
+        if (piecePosition.equals(kingPosition.get())) {
             return isValidKnightMove(attackerPosition, piecePosition);
-        } else if (piecePosition.equals(knightPosition)) {
+        } else if (piecePosition.equals(knightPosition.get())) {
             return isValidKingMove(attackerPosition, piecePosition);
         }
         return false;
     }
 
-    // Public methods (state checks and core logic)
     @Override
     public boolean isSolved() {
-        return kingPosition.equals(new Position(0, 6)) || knightPosition.equals(new Position(0, 6));
+        return solved.get();
     }
 
     @Override
     public boolean isLegalToMoveFrom(Position position) {
-        if (position.equals(kingPosition)) {
-            return isUnderAttack(kingPosition, knightPosition);
-        } else if (position.equals(knightPosition)) {
-            return isUnderAttack(knightPosition, kingPosition);
+        if (position.equals(kingPosition.get())) {
+            return isUnderAttack(kingPosition.get(), knightPosition.get());
+        } else if (position.equals(knightPosition.get())) {
+            return isUnderAttack(knightPosition.get(), kingPosition.get());
         }
         return false;
     }
@@ -83,11 +94,11 @@ public class ChessState implements TwoPhaseMoveState<Position> {
         Position from = move.from();
         Position to = move.to();
         if (isValid(to)) {
-            if (from.equals(knightPosition) && isValidKnightMove(knightPosition, to)) {
-                return isUnderAttack(knightPosition, kingPosition);
+            if (from.equals(knightPosition.get()) && isValidKnightMove(knightPosition.get(), to)) {
+                return isUnderAttack(knightPosition.get(), kingPosition.get());
             }
-            if (from.equals(kingPosition) && isValidKingMove(kingPosition, to)) {
-                return isUnderAttack(kingPosition, knightPosition);
+            if (from.equals(kingPosition.get()) && isValidKingMove(kingPosition.get(), to)) {
+                return isUnderAttack(kingPosition.get(), knightPosition.get());
             }
         }
         return false;
@@ -101,30 +112,30 @@ public class ChessState implements TwoPhaseMoveState<Position> {
 
         Position from = move.from();
         Position to = move.to();
-        if (from.equals(kingPosition)) {
-            kingPosition = to;
-        } else if (from.equals(knightPosition)) {
-            knightPosition = to;
+        if (from.equals(kingPosition.get())) {
+            board.get(kingPosition.get().row())[kingPosition.get().col()] = ".";
+            kingPosition.set(to);
+            board.get(kingPosition.get().row())[kingPosition.get().col()] = "K";
+        } else if (from.equals(knightPosition.get())) {
+            board.get(knightPosition.get().row())[knightPosition.get().col()] = ".";
+            knightPosition.set(to);
+            board.get(knightPosition.get().row())[knightPosition.get().col()] = "N";
         }
-
     }
 
-    // Public methods (move generation)
     @Override
     public Set<TwoPhaseMove<Position>> getLegalMoves() {
         Set<TwoPhaseMove<Position>> legalMoves = new HashSet<>();
-
         collectKingMoves(legalMoves);
         collectKnightMoves(legalMoves);
-
         return legalMoves;
     }
 
     private void collectKingMoves(Set<TwoPhaseMove<Position>> legalMoves) {
         for (Direction direction : Direction.values()) {
             if (direction.isKingMove()) {
-                Position newKingPosition = kingPosition.move(direction);
-                TwoPhaseMove<Position> move = new TwoPhaseMove<>(kingPosition, newKingPosition);
+                Position newKingPosition = kingPosition.get().move(direction);
+                TwoPhaseMove<Position> move = new TwoPhaseMove<>(kingPosition.get(), newKingPosition);
                 if (isLegalMove(move)) {
                     legalMoves.add(move);
                 }
@@ -135,8 +146,8 @@ public class ChessState implements TwoPhaseMoveState<Position> {
     private void collectKnightMoves(Set<TwoPhaseMove<Position>> legalMoves) {
         for (Direction direction : Direction.values()) {
             if (direction.isKnightMove()) {
-                Position newKnightPosition = knightPosition.move(direction);
-                TwoPhaseMove<Position> move = new TwoPhaseMove<>(knightPosition, newKnightPosition);
+                Position newKnightPosition = knightPosition.get().move(direction);
+                TwoPhaseMove<Position> move = new TwoPhaseMove<>(knightPosition.get(), newKnightPosition);
                 if (isLegalMove(move)) {
                     legalMoves.add(move);
                 }
@@ -144,10 +155,9 @@ public class ChessState implements TwoPhaseMoveState<Position> {
         }
     }
 
-    // Overridden methods
     @Override
     public ChessState clone() {
-        return new ChessState(kingPosition, knightPosition);
+        return new ChessState(kingPosition.get(), knightPosition.get());
     }
 
     @Override
@@ -156,17 +166,29 @@ public class ChessState implements TwoPhaseMoveState<Position> {
         if (!(o instanceof ChessState other)) {
             return false;
         }
-        return kingPosition.equals(other.kingPosition) &&
-                knightPosition.equals(other.knightPosition);
+        return kingPosition.get().equals(other.kingPosition.get()) &&
+                knightPosition.get().equals(other.knightPosition.get());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(kingPosition, knightPosition);
+        return Objects.hash(kingPosition.get(), knightPosition.get());
     }
 
     @Override
     public String toString() {
-        return "King: " + kingPosition + ", Knight: " + knightPosition;
+        return "King: " + kingPosition.get() + ", Knight: " + knightPosition.get();
+    }
+
+    public ReadOnlyObjectWrapper<Position> kingPositionProperty() {
+        return kingPosition;
+    }
+
+    public ReadOnlyObjectWrapper<Position> knightPositionProperty() {
+        return knightPosition;
+    }
+
+    public ReadOnlyBooleanProperty solvedProperty() {
+        return solved.getReadOnlyProperty();
     }
 }
